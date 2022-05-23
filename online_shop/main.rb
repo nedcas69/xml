aeb7365268df280629d6edd661ffc8f87dd09b19
Shax <nedcas69@gmail.com>
2022-05-23 09:28:57 +0500

    oK

---
+++ online_shop/main.rb
@@ -0,0 +1,29 @@
+# Как всегда подключаем парсер
+require 'rexml/document'
+
+require_relative 'product'
+require_relative 'book'
+require_relative 'disk'
+require_relative 'movie'
+
+total_price = 0
+
+# Всё, что мы поменяем, это то, как мы получаем массив продуктов
+# Раньше мы генерили его руками, теперь этим будет заниматься статический метод
+# read_from_xml, которому мы будем передавать относительный путь к файлу
+products = Product.read_from_xml('data/products.xml')
+
+choice = nil
+
+while choice != 'x'
+  Product.showcase(products)
+
+  choice = STDIN.gets.chomp
+
+  if choice != 'x' && choice.to_i < products.size && choice.to_i >= 0
+    product = products[choice.to_i]
+    total_price += product.buy
+  end
+end
+
+puts "Спасибо за покупки, с Вас #{total_price} руб."
